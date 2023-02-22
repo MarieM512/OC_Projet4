6de854988b3d639a7faba67e2658a5b6ba8bd526
Marie Metay <mariemetay.pro@gmail.com>
2023-02-22 22:13:06 +0100

    Add calendar dialog

--- app/src/main/java/com/metay/mareu/ui/meeting_list/AddMeetingActivity.java
+++ app/src/main/java/com/metay/mareu/ui/meeting_list/AddMeetingActivity.java
@@ -1,12 +1,14 @@
 package com.metay.mareu.ui.meeting_list;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -14,18 +16,23 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.projet4.R;
 import com.example.projet4.databinding.ActivityAddMeetingBinding;
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.metay.mareu.MainActivity;
 import com.metay.mareu.injection.ViewModelFactory;
 import com.metay.mareu.model.Meeting;
 import com.metay.mareu.model.Room;
 import com.metay.mareu.ui.meeting_list.viewmodel.MainViewModel;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddMeetingActivity extends AppCompatActivity {
 
     private ActivityAddMeetingBinding binding;
+    final Calendar myCalendar= Calendar.getInstance();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +60,24 @@ public class AddMeetingActivity extends AppCompatActivity {
             }
         });
 
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+
+        binding.inputDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddMeetingActivity.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
         MainViewModel model = new ViewModelProvider(this, ViewModelFactory.getInstance(this)).get(MainViewModel.class);
 
         binding.btnCreate.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +96,12 @@ public class AddMeetingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void updateLabel(){
+        String myFormat="dd/MM/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.FRANCE);
+        binding.inputDate.setText(dateFormat.format(myCalendar.getTime()));
     }
 
     /**
