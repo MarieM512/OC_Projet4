4cfd0783d0f04c94236f20bbf89e2fda05b8d785
Marie Metay <mariemetay.pro@gmail.com>
2023-03-03 10:50:19 +0100

    Fix MeetingListAdapter with binding

--- app/src/main/java/com/metay/mareu/ui/meeting_list/MeetingListAdapter.java
+++ app/src/main/java/com/metay/mareu/ui/meeting_list/MeetingListAdapter.java
@@ -3,23 +3,19 @@ package com.metay.mareu.ui.meeting_list;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.projet4.R;
-import com.metay.mareu.api.MeetingApiService;
+import com.example.projet4.databinding.ItemRecycleviewBinding;
 import com.metay.mareu.model.Meeting;
-import com.metay.mareu.model.Room;
 
 public class MeetingListAdapter extends ListAdapter<Meeting, MeetingListAdapter.MeetingViewHolder> {
 
     MeetingInterface mMeetingInterface;
+    ItemRecycleviewBinding binding;
 
     public MeetingListAdapter(@NonNull DiffUtil.ItemCallback<Meeting> diffCallback, MeetingInterface meetingInterface) {
         super(diffCallback);
@@ -29,41 +25,29 @@ public class MeetingListAdapter extends ListAdapter<Meeting, MeetingListAdapter.
     @NonNull
     @Override
     public MeetingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MeetingViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycleview, parent, false));
+        return new MeetingViewHolder(ItemRecycleviewBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MeetingViewHolder holder, int position) {
         Meeting meeting = getItem(position);
-        holder.bind(meeting);
+        String title = meeting.getName() + " - " + meeting.getTime() + " - " + meeting.getRoom();
+        binding.itemName.setText(title);
+        binding.itemMembers.setText(meeting.getGuests());
+        binding.itemImage.setImageResource(meeting.getImgRoom());
+        binding.itemDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMeetingInterface.removeMeeting(meeting);
+            }
+        });
     }
 
     class MeetingViewHolder extends RecyclerView.ViewHolder {
 
-        TextView meeting;
-        TextView members;
-        ImageButton deleteButton;
-        ImageView room;
-
-        public MeetingViewHolder(@NonNull View itemView) {
-            super(itemView);
-            meeting = itemView.findViewById(R.id.item_name);
-            members = itemView.findViewById(R.id.item_members);
-            deleteButton = itemView.findViewById(R.id.item_delete_button);
-            room = itemView.findViewById(R.id.item_image);
-        }
-
-        public void bind(Meeting meeting) {
-            String title = meeting.getName() + " - " + meeting.getTime() + " - " + meeting.getRoom();
-            this.meeting.setText(title);
-            members.setText(meeting.getGuests());
-            room.setImageResource(meeting.getImgRoom());
-            deleteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mMeetingInterface.removeMeeting(meeting);
-                }
-            });
+        public MeetingViewHolder(ItemRecycleviewBinding b) {
+            super(b.getRoot());
+            binding = b;
         }
     }
 }
